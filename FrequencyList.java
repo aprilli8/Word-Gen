5f33a4eedb789e9a11464e9a68f70c6da6365d96
23ayl2 <23ayl2@tcl216-6.cs.williams.edu>
2019-09-18 16:11:08 -0400

    Need to fix addFreqElement method in Table class

--- FrequencyList.java
+++ FrequencyList.java
@@ -1,3 +1,37 @@
-public class FrequencyList {
-  
+import structure5.*;
+import java.util.Scanner;
+import java.util.Random;
+
+public class FrequencyList{
+  private Vector<Association<String, Integer>> thirdChar;
+
+  public FrequencyList(String letter){
+    thirdChar = new Vector<Association<String, Integer>>();
+    thirdChar.add(new Association<String, Integer>(letter, new Integer(1)));
+  }
+
+  public String getLetter(int index){
+    return (thirdChar.get(index)).getKey();
+  }
+
+  public int getCount(int index){
+    return (thirdChar.get(index)).getValue();
+  }
+
+  public void addCount(String let){
+    for(int i = 0; i < thirdChar.size(); i++){
+      if(((thirdChar.get(i)).getKey()).equals(let) == true){
+        (thirdChar.get(i)).setValue((thirdChar.get(i)).getValue()+1);
+      }
+    }
+  }
+/*
+  public static void main(String[] args){
+    FrequencyList test = new FrequencyList("a");
+    System.out.println(test.getLetter(0));
+    System.out.println(test.getCount(0));
+    test.addCount("a");
+    System.out.println(test.getCount(0));
+  }
+*/
 }
--- Table.java
+++ Table.java
@@ -1,3 +1,42 @@
+import structure5.*;
+import java.util.Scanner;
+import java.util.Random;
+
 public class Table{
-  
+  private Vector<Association<String, FrequencyList>> initialChars;
+
+  public Table(String letters, String nextLet){
+    initialChars = new Vector<Association<String, FrequencyList>>();
+    initialChars.add(new Association<String, FrequencyList>(letters, new FrequencyList(nextLet)));
+  }
+
+  public String getInitialChars(int index){
+    return (initialChars.get(index)).getKey();
+  }
+
+  public String getNextChar(int index, int ind){
+    return ((initialChars.get(index)).getValue()).getLetter(ind);
+  }
+
+  public int getFreqCount(int index, int ind){
+    return ((initialChars.get(index)).getValue()).getCount(ind);
+  }
+
+  public void addFreqElement(String firstLets, String nextLet){
+    for(int i = 0; i < initialChars.size(); i++){
+      if(((initialChars.get(i)).getKey()).equals(firstLets)){
+        (initialChars.get(i)).setValue(new FrequencyList(nextLet));
+      }
+    }
+  }
+
+  public static void main(String[] args){
+    Table test = new Table("ab", "c");
+    System.out.println(test.getInitialChars(0));
+    System.out.println(test.getNextChar(0,0));
+    System.out.println(test.getFreqCount(0,0));
+    test.addFreqElement("ab", "d");
+    System.out.println(test.getNextChar(0,1));
+    System.out.println(test.getFreqCount(0,1));
+  }
 }
